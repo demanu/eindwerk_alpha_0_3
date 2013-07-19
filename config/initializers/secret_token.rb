94dd210fdb6256a02469233e78c453643bb7e278
demanu <emmanuel.verbraecken@gmail.com>
2013-07-19 19:57:18 +0200

    leeg project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EindwerkAlpha03::Application.config.secret_token = 'f8b850bf074a9288695b49c2914162317f78c8940540c985f399aeb9e4da07b0cf28da8c1378411aeaadf3f41087b0918c7f5f0f1a7df64f97d9e72db0bcdfb8'
